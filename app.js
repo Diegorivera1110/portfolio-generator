97e51a8177a7cebee40e6e3e4abde388564790be
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-05-15 13:06:44 -0700

    initialized inquirer

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
-const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-// console.log(profileDataArgs);
+const inquirer = require("inquirer");
 
 
-// const printProfileData = profileDataArr => {
-//     // This ...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-        
-//         console.log(profileDataArr[i]);
-//     }
+// const fs = require('fs');
 
-//     console.log("================");
+// const generatePage = require('./src/page-template.js');
 
-//     // is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
+// const pageHTML = generatePage(name, github);
 
-// printProfileData(profileDataArgs);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer 
+.prompt([
+  {
+    type: 'input', 
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+.then(answers => console.log(answers));

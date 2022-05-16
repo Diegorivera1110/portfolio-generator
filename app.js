bc775951fe1291ca5d415e4ee6efe29bc365b184
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-05-15 17:57:52 -0700

    added HTML template

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const inquirer = require("inquirer");
 
-// const fs = require('fs');
+const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
@@ -148,5 +148,14 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+        // const pageHTML = generatePage();
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+
   });
